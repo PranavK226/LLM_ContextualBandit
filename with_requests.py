6f8cf62f574dcfb8e442da956ee514c0dbe9ba14
PranavK226 <pranav.kannepalli@gmail.com>
2023-10-21 23:23:16 -0700

    embeddings w/ different sentence transformer

--- with_requests.py
+++ with_requests.py
@@ -2,19 +2,31 @@ import requests
 import pandas as pd
 
 print("variables")
-model_id = "codellama/CodeLlama-7b-hf"
-hf_token = "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"
-api_url = f"https://api-inference.huggingface.co/pipeline/feature-extraction/{model_id}"
-headers = {"Authorization": f"Bearer {hf_token}"}
+model_name = "sentence-transformers/all-MiniLM-L6-v2"
+api_key = "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"
 
-print("function def")
-def query(texts):
-    response = requests.post(api_url, headers=headers, json={"inputs": texts, "options":{"wait_for_model":True}})
-    return response.json()
+def encode_text(text):
+  endpoint_url = f"https://api-inference.huggingface.co/pipeline/feature-extraction/{model_name}"
+  headers = {"Authorization": f"Bearer {api_key}"}
+  payload = {"inputs": text}
+  response = requests.post(endpoint_url, headers=headers, json=payload)
+  if response.status_code == 200:
+    data = response.json()
+    return data
+  else:
+    raise Exception(f"Request failed with status code {response.status_code}")
 
-texts = ["Does this work?"]
+text1 = "I love llamas"
+text2 = "Llamas are awesome"
+text3 = "Cats are cute"
 
-print("running queries")
-output = query(texts)
+embedding1 = encode_text(text1)
+embedding2 = encode_text(text2)
+embedding3 = encode_text(text3)
 
-print(output)
+print("Embedding for '{}':".format(text1))
+print(embedding1)
+print("Embedding for '{}':".format(text2))
+print(embedding2)
+print("Embedding for '{}':".format(text3))
+print(embedding3)
