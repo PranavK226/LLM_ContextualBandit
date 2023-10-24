b07deb53d36765bd922164cd21c5ff5babbd7f7e
PranavK226 <pranav.kannepalli@gmail.com>
2023-10-23 19:09:36 -0700

    using pipelines

--- pipeline.py
+++ pipeline.py
@@ -3,7 +3,7 @@ from transformers import pipeline
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
 tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-hf", token="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")
-#model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-2-7b-hf")
+model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-2-7b-hf", token="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")
 
 texts = ["Does this work?"]
 print(tokenizer(texts))
